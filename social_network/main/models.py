ef9cc22755a863b4e5d123201228bf694b7949f4
scad89 <scad200@gmail.com>
2022-05-28 05:06:53 +0300

    added models and migrations

---
+++ social_network/main/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+
+class LikeDislike(models.Model):
+    class Like(models.TextChoices):
+        like = 'like'
+        dislike = 'dislike'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='likes')
+    like = models.CharField(choices=Like.choices, max_length=7)
+
+    def __str__(self):
+        return f'{self.user}, {self.post}, {self.like}'
+
+    class Meta:
+        verbose_name = 'Лайк'
+        verbose_name_plural = 'Лайки'
